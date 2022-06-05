c5df16d35fdf0e99452c164717d187884aec39db
Andrew Chambers <ac@acha.ninja>
2022-06-05 16:01:00 +1200

    Format.

--- example/lclient/src/main.rs
+++ example/lclient/src/main.rs
@@ -1,11 +1,11 @@
-use p92000::lclient;
 use p92000::fcall::LOpenFlags;
+use p92000::lclient;
 // use std::io::{Read, Write};
 use std::net::TcpStream;
 
 fn main() {
     let conn = TcpStream::connect("127.0.0.1:5030").unwrap();
-    let client = lclient::Client::tcp(conn, 128*1024).unwrap();
+    let client = lclient::Client::tcp(conn, 128 * 1024).unwrap();
 
     let (_, f) = client.attach(0, "ac", "/tmp").unwrap();
     f.open(LOpenFlags::O_RDONLY).unwrap();
